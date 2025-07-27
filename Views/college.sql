f2ed76f787fddfd6e6e72b52b24ae23302181b5d
Sendy2626 <sendy.hartono@gmail.com>
2025-07-28 00:12:01 +0700

    Commt on dev

---
+++ Views/college.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW `college`
+AS
+	SELECT
+	  `students`.`ID` AS `ID`,
+	  `students`.`first_name` AS `first_name`,
+	  `students`.`last_name` AS `last_name`,
+	  `students`.`date_of_birth` AS `date_of_birth`
+	FROM `students`;
